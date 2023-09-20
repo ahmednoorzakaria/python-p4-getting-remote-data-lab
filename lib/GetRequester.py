a38312188587033725b4fdd7ec9521d5ee8c7775
ahmednoorzakaria <ahmednoor.zakaria@student.moringaschool.com>
2023-09-20 17:46:46 +0300

    API

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,12 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        data_list = []
+        data = json.loads(self.get_response_body())
+        for datas in data:
+            data_list.append({datas['name'],datas['occupation']})
+            return data_list
